893d3b4dd1d2ed61f07347f9c20f940424f42ccf
Conner Lunn <connerlunn@gmail.com>
2020-05-31 15:40:01 -0500

    redux now holds board state

--- src/index.js
+++ src/index.js
@@ -3,22 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {isValidSudoku} from './sudoku';
 
 //Redux Code
-
 const defaultState = {
     grid: [[]]
 };
 
-const reducer = (grid = defaultState, action) => {
+const reducer = (state = defaultState, action) => {
     if (action.type === "NUM-CHANGE") {
+        let newgrid = Object.assign([...state.grid], {
+            [action.y]: Object.assign([...state.grid[action.y]], {
+              [action.x]: action.value
+            })
+          })
         return {
-            grid: grid[action.y][action.x] = action.value
+            grid: newgrid
         };
+    } else if (action.type === 'SET-BOARD'){
+        return{
+            grid: action.grid
+        };
+    } else {
+        return state;
     }
 };
 
 const store = createStore(reducer);
+
+const setBoardAction = (board) => {
+    return {
+        type: 'SET-BOARD',
+        grid: board
+    }
+};
 
 const numChangeAction = (psx,psy,val) => {
     return {
@@ -29,6 +49,18 @@ const numChangeAction = (psx,psy,val) => {
     }
 };
 
+const mapStateToProps = (state) => {
+    return { grid: state.grid };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        numChange: (x,y,val) => dispatch(
+            numChangeAction(x,y,val)
+        )
+    }
+}
+
 //React Code
 class Cell extends React.Component {
     constructor(props) {
@@ -36,20 +68,31 @@ class Cell extends React.Component {
         this.state = {
             x: this.props.posx,
             y: this.props.posy,
-            value: this.props.value,    
-            isGiven: this.props.value,
+            isGiven: this.props.isGiven,
         };
         this.clicked = this.clicked.bind(this);
     }
 
     clicked() {
-        var val = this.state.value
+        var val = this.props.grid[this.state.y][this.state.x]
         if (val < 9 & val !== '') {
-            this.setState({value: val + 1})
+            this.props.numChange(
+                this.state.x,
+                this.state.y,
+                val + 1,
+            )
         } else if (val === 9) {
-            this.setState({value: ''})
+            this.props.numChange(
+                this.state.x,
+                this.state.y,
+                '',
+            )
         } else if (val === '') {
-            this.setState({value: 1})
+            this.props.numChange(
+                this.state.x,
+                this.state.y,
+                1,
+            )
         }
     }
 
@@ -57,14 +100,14 @@ class Cell extends React.Component {
         if (this.state.isGiven) {
             return (
                 <td className="isGiven">
-                    {this.state.value}
+                    {this.props.grid[this.state.y][this.state.x]}
                 </td>
             );
         } else {
             return (
                 <td>
                     <button className='cell-button' onClick={this.clicked}>
-                        {this.state.value} 
+                        {this.props.grid[this.state.y][this.state.x]}
                     </button>
                 </td>
             );
@@ -72,21 +115,24 @@ class Cell extends React.Component {
     }
 }
 
+const Celll = connect(mapStateToProps, mapDispatchToProps)(Cell);
+
 class Grid extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        var grid = this.props.gridNums
+        var grid = store.getState().grid
         grid = grid.map(
             (row, y) => {
                 return row.map(
-                    (num, x) => {
-                        if(num === 0) {
-                            return <Cell value={''} posx={x} posy={y} />
+                    (item, x) => {
+                        if(item === '') {
+                            //store.dispatch(numChangeAction(x,y,''))
+                            return <Celll posx={x} posy={y} isGiven={false}/>
                         } else {
-                            return <Cell value={num} posx={x} posy={y} />
+                            return <Celll posx={x} posy={y} isGiven={true}/>
                         }
                     }
                 );
@@ -129,70 +175,33 @@ class Sudoku extends React.Component {
 
     fetchSudoku() {
         var puzzle = [
-            [0,6,0,3,0,0,8,0,4],
-            [5,3,7,0,9,0,0,0,0],
-            [0,4,0,0,0,6,3,0,7],
-            [0,9,0,0,5,1,2,3,8],
-            [0,0,0,0,0,0,0,0,0],
-            [7,1,3,6,2,0,0,4,0],
-            [3,0,6,4,0,0,0,1,0],
-            [0,0,0,0,6,0,5,2,3],
-            [1,0,2,0,0,9,0,8,0]];
-        return puzzle
+            ['', 6,'', 3,'','', 8,'', 4],
+            [5 , 3, 7,'', 9,'','','',''],
+            ['',4,'','','',6,3,'',7],
+            ['',9,'','',5,1,2,3,8],
+            ['','','','','','','','',''],
+            [7,1,3,6,2,'','',4,''],
+            [3,'',6,4,'','','',1,''],
+            ['','','','',6,'',5,2,3],
+            [1,'',2,'','',9,'',8,'']];
+
+        store.dispatch(setBoardAction(puzzle))
     }
     
     // returns true if arraySolution is valid, false otherwise
-    /*
     checkSolution() {
-        var arraySolution = store.getState()
-        for (var y = 0; y < 9; ++y) {
-            for (var x = 0; x < 9; ++x) {
-                var value = arraySolution[y][x];
-                if (value) {
-                    if(value === ''){
-                        return false;
-                    }
-                    // Check the line
-                    for (var x2 = 0; x2 < 9; ++x2) {
-                        if (x2 != x && arraySolution[y][x2] == value) {
-                            return false;
-                        } 
-                    }
-                    // Check the column
-                    for (var y2 = 0; y2 < 9; ++y2) {
-                        if (y2 != y && arraySolution[y2][x] == value) {
-                            return false;
-                        } 
-                    }
-                    // Check the square
-                    var startY = Math.floor(y/3)*3;
-                    for (var y2 = startY; y2 < startY + 3; ++y2) {
-                        var startX = Math.floor(x/3)*3;
-                        for (x2 = startX; x2 < startX + 3; ++x2) {
-                            if ((x2 != x || y2 != y) && arraySolution[y2][x2] == value) {
-                                return false;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    */
-
-    newSudoku () {
-        var newSudoku = this.fetchSudoku()
-        //apply changes to board
+        const valid = isValidSudoku(store.grid)
+        console.log(valid)
     }
 
     render() {
+        this.fetchSudoku()
         return ( 
             <div >
                 <h1>React/Redux Sudoku App</h1>
-                <Grid gridNums={this.fetchSudoku()}  />
+                <Grid />
                 <div className='inline'>
-                    <button className='bottomButtons'>New Sudoku</button>
+                    <button onClick={this.fetchSudoku} className='bottomButtons'>New Sudoku</button>
                     <button className='bottomButtons'>Check Solution</button>
                 </div>
             </div>
